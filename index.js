614c942102ff47361da4d8560618eeb3e9601cba
Philipp Fehre <philipp.fehre@googlemail.com>
2013-08-24 09:04:56 +0200

    Test setup and first sanity test
    using jasmine node, and request to get the first roundtrip test passing

---
+++ index.js
@@ -0,0 +1,6 @@
+var nurl = function(req, res) {
+  res.end()
+}
+
+module.exports = nurl
+
---
+++ spec/api.spec.js
@@ -0,0 +1,49 @@
+// SETUP
+process.env.NODE_ENV = "test"
+process.env.PORT = 9876
+
+var app = require("../index.js")
+var http = require("http")
+var request = require("request")
+
+// Automatic testserver setup and shutdown between tests
+var server, testUrl
+var loadTestServer = function() {
+  var hookBeforeEach = function(fn) {
+    jasmine.getEnv().currentSuite.beforeEach(fn)
+  }
+
+  var hookAfterEach = function(fn) {
+    jasmine.getEnv().currentSuite.afterEach(fn)
+  }
+
+  hookBeforeEach(function() {
+    var running = false
+    var port = process.env.PORT
+
+    runs(function() {
+      server = http.createServer(app).listen(port, function() {
+        testUrl = "http://localhost:" + port
+        running = true
+      })
+    })
+
+    waitsFor(function() { return running })
+  })
+
+  hookAfterEach(function() { server.close() })
+}
+
+// SPECS
+describe("POST '/'", function(){
+  loadTestServer()
+
+  it("responds with 200", function(done) {
+    var q = { url: testUrl, json: { u: "http://google.com" } }
+    request.post(q, function(err, res, body) {
+      expect(res.statusCode).toBe(200)
+      done()
+    })
+  })
+})
+
