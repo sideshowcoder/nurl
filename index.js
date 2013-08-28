0ea19fa8b88c5f630254d64a8d1f3f975070afb2
Philipp Fehre <philipp.fehre@googlemail.com>
2013-08-28 09:45:50 +0200

    StreamParse request stream result

--- index.js
+++ index.js
@@ -13,16 +13,56 @@ Router.prototype.addRoute = function(regex, method, fn) {
 }
 
 Router.prototype.dispatch = function(req, res) {
-  this.routes[req.method].forEach(function(matcher) {
-    if(matcher.path.test(req.url)) matcher.func(req, res)
-  })
+  var methods = this.routes[req.method]
+  for(var i = 0, method = methods[i]; method = methods[i]; i++) {
+    if(method.path.test(req.url)) return method.func(req, res)
+  }
+
   res.statusCode = 404
   res.end()
 }
 
+// Body Parser
+var util = require("util")
+var Transform = require("stream").Transform
+var BodyParser = function() {
+  Transform.call(this, {})
+  this._body= ""
+  this._readableState.objectMode = true;
+  this._writableState.objectMode = false;
+}
+util.inherits(BodyParser, Transform)
+
+BodyParser.prototype._transform = function(chunk, encoding, done) {
+  this._body += chunk
+  done()
+}
+
+BodyParser.prototype._flush = function(fn) {
+  this.push(JSON.parse(this._body))
+  fn()
+}
+
+// Result Stringify
+var ResultStringify = function() {
+  Transform.call(this, {})
+  this._readableState.objectMode = false;
+  this._writableState.objectMode = true;
+}
+util.inherits(ResultStringify, Transform)
+
+ResultStringify.prototype._transform = function(chunk, encoding, done) {
+  this.push(JSON.stringify(chunk))
+  done()
+}
+
 // Handle requests
 var createShortUrl = function(req, res) {
-  res.end("nurl")
+  var bodyParser = new BodyParser()
+  var stringify = new ResultStringify()
+  req.pipe(bodyParser)
+     .pipe(stringify)
+     .pipe(res)
 }
 
 var router = new Router()
--- spec/api.spec.js
+++ spec/api.spec.js
@@ -2,6 +2,14 @@
 process.env.NODE_ENV = "test"
 process.env.PORT = 9876
 
+// make sure we don't simply crash when an exception happens
+// ie a matcher function is not defined, so we can actually debug it
+var sys = require('sys')
+process.on('uncaughtException',function(e) {
+  sys.log("Caught unhandled exception: " + e);
+  sys.log(" ---> : " + e.stack);
+});
+
 var app = require("../index.js")
 var http = require("http")
 var request = require("request")
@@ -40,26 +48,33 @@ describe("POST '/'", function(){
 
   it("responds with 200", function(done) {
     var q = { url: testUrl, json: { u: "http://google.com" } }
-    request.post(q, function(err, res, body) {
-      expect(res.statusCode).toBe(200)
-      done()
-    })
+      request.post(q, function(err, res, body) {
+        expect(res.statusCode).toBe(200)
+        done()
+      })
   })
 
   it("errors on the wrong url", function(done) {
     var q = { url: testUrl + "/foo/bar", json: { u: "http://google.com" } }
-    request.post(q, function(err, res, body) {
-      expect(res.statusCode).toBe(404)
-      done()
-    })
+      request.post(q, function(err, res, body) {
+        expect(res.statusCode).toBe(404)
+        done()
+      })
   })
 
   it("errors on the wrong method", function(done) {
-    var q = { url: testUrl + "/", json: { u: "http://google.com" } }
-    request.put(q, function(err, res, body) {
-      expect(res.statusCode).toBe(404)
-      done()
-    })
+    var q = { url: testUrl, json: { u: "http://google.com" } }
+      request.put(q, function(err, res, body) {
+        expect(res.statusCode).toBe(404)
+        done()
+      })
+  })
+
+  it("creates a short url", function(done) {
+    var q = { url: testUrl, json: { u: "http://google.com" } }
+      request.post(q, function(err, res, body) {
+        done()
+      })
   })
 
 })
